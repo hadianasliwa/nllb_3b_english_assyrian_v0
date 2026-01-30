d0f478701df4e7f62c641308b123a459a4debdbf
hadiana sliwa <hadiana.eng@gmail.com>
2026-01-30 20:41:20 +0300

    ignore

--- upload.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import HfApi
-api = HfApi()
-
-api.upload_folder(
-    folder_path="nllb_eng_aii_lora",
-    repo_id="hadiana/nllb_3b_english_assyrian_v0",
-    repo_type="model",
-)
